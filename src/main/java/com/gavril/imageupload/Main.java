1e913242898651655b061a95fd80dfeb69bb6d30
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-12-16 18:27:23 +0200

    get directory content

--- src/main/java/com/gavril/imageupload/Main.java
+++ src/main/java/com/gavril/imageupload/Main.java
@@ -3,10 +3,21 @@ package com.gavril.imageupload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+
+		// GET list of files in directory
+		String[] pathnames;
+		File f = new File("src/main/resources/images/");
+		pathnames = f.list();
+
+		for (String pathname : pathnames) {
+			System.out.println(pathname);
+		}
 	}
 }
